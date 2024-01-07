097e14b3216df48b9fd19adf90f12b534f8f1e1d
Haris Fadhilah <hf130103@gmail.com>
2024-01-07 22:49:50 +0700

    admin page

--- Admin.php
+++ Admin.php
@@ -5,7 +5,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>TECLACY</title>
+  <!-- CSS Utama -->
+  <link rel="stylesheet" href="assets/admin.css" />
+  <!-- Script Utama -->
+  <script src="js/script.js"></script>
 
+  <!-- Google Font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@500;700&family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+    rel="stylesheet" />
 </head>
 
 <body>
@@ -27,7 +37,10 @@
       </ul>
     </div>
   </aside>
-  <main></main>
+  <main>
+    <header>Update Study Flow</header>
+    <section></section>
+  </main>
   <script>
   feather.replace();
   </script>
