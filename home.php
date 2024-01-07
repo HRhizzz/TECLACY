1d95ad23d1dfa77fd500062eff7741ebf05de921
Haris Fadhilah <hf130103@gmail.com>
2024-01-07 19:06:46 +0700

    fix

--- home.php
+++ home.php
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -44,36 +48,34 @@
           <a href="signin.php" onclick="toggleProfile()">Sign In</a>
         </li>
         <li class="profile-button" id="profile" style="display: none">
-          <a href="#">Profil</a>
+          <div><i data-feather="circle"></i></div><a href="#">Profil</a>
         </li>
       </ul>
     </div>
   </header>
   <div class="body-container">
     <aside>
-      <div class="sidebox">
-        <div class="sidebar">
-          <ul>
-            <li class="search-sidebar" id="search-all">
-              <a href="#">Search</a>
-            </li>
-            <li class="destiny-flow" id="Destiny-Flow">
-              <a href="#">Destiny Flow</a>
-            </li>
-            <li class="study-flow" id="Study-Flow">
-              <a href="#">Study Flow</a>
-            </li>
-          </ul>
-        </div>
+      <div class="sidebar">
+        <ul>
+          <li class="search-sidebar" id="search-all">
+            <a href="#"> Search</button>
+          </li>
+          <li class="destiny-flow" id="Destiny-Flow">
+            <a href="#"> Destiny Flow</a></button>
+          </li>
+          <li class="study-flow" id="Study-Flow">
+            <a href="#">Study Flow</a></button>
+          </li>
+        </ul>
       </div>
     </aside>
     <main>
       <div id="landing-page" class="landing-page">
-        <div class="box-bg-landing-page">
-          <div class="welcome-text-landing-page">
+        <div class="hero-bg">
+          <div class=" hero-welcome">
             <p>Selamat datang di Teclacy!</p>
           </div>
-          <div class="description-text-landing-page">
+          <div class="hero-description">
             <p>
               Tempat yang memandu langkah Anda menuju karir yang cerah di
               dunia Teknologi Informasi. Temukan arah dan panduan yang tepat
@@ -84,41 +86,47 @@
             </p>
           </div>
         </div>
-        <div class="landing-page">
-          <div class="welcome-text-landing-page">
-            <p>Selamat datang di Teclacy!</p>
+      </div>
+      <div class="fitur-landing-page">
+        <div class="gambar-fitur"></div>
+        <div class="studyflow-text">
+          <div class="studyflow-welcome">
+            <p>Study Flow: Mengalir Bersama Minat Anda</p>
           </div>
-          <div class="description-text-landing-page">
+          <div class="studyflow-description">
             <p>
-              Tempat yang memandu langkah Anda menuju karir yang cerah di
-              dunia Teknologi Informasi. Temukan arah dan panduan yang tepat
-              untuk mengembangkan potensi Anda dalam industri yang terus
-              berkembang ini. Mari menjelajahi peluang, mendapatkan wawasan,
-              dan meraih kesuksesan dalam karir TI yang sesuai dengan minat
-              dan keahlian Anda.
+              Dengan fitur unggulan kami, "Study Flow," Anda akan menemukan panduan belajar yang sesuai dengan minat
+              Anda. Bagi kami, setiap minat adalah kunci untuk membuka potensi Anda dalam bidang IT. Melalui tag minat
+              yang tersedia, Anda akan diarahkan ke alur belajar yang cocok dengan minat Anda, menjadikan proses belajar
+              lebih menarik dan relevan. Temukan koneksi antara minat Anda dengan beragam bidang IT di sampingnya.
             </p>
           </div>
         </div>
-        <div class="landing-page">
-          <div class="welcome-text-landing-page">
-            <p>Selamat datang di Teclacy!</p>
+      </div>
+      <div class="fitur-landing-page">
+        <div class="gambar-fitur"></div>
+        <div class="destinyflow-text">
+          <div class="destinyflow-welcome">
+            <p>Destiny Flow: Takdir Pendidikan Anda</p>
           </div>
-          <div class="description-text-landing-page">
+          <div class="destinyflow-description">
             <p>
-              Tempat yang memandu langkah Anda menuju karir yang cerah di
-              dunia Teknologi Informasi. Temukan arah dan panduan yang tepat
-              untuk mengembangkan potensi Anda dalam industri yang terus
-              berkembang ini. Mari menjelajahi peluang, mendapatkan wawasan,
-              dan meraih kesuksesan dalam karir TI yang sesuai dengan minat
-              dan keahlian Anda.
+              Tak hanya itu, dengan "Destiny Flow," kami hadirkan solusi pencarian yang efisien. Cukup masukkan tag
+              minat yang Anda miliki, dan kami akan menyajikan berbagai pilihan "Study Flow" yang telah difilter sesuai
+              minat yang Anda inputkan. Ini adalah pintu untuk menggali lebih dalam dan menemukan jalur pendidikan ideal
+              Anda dalam dunia IT.
             </p>
           </div>
         </div>
+      </div>
     </main>
   </div>
   <footer>
     <p>Teclacy IT for FUTURE</p>
   </footer>
+  <script>
+  feather.replace();
+  </script>
 </body>
 
 </html>
